af20f4b2377af128df93ec7eeaed88765dad806f
Jojo Mampilly <mampillyjojo@gmail.com>
2016-08-03 20:14:15 -0700

    Tip Calculator first Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by jojo mampilly on 8/3/16.
+//  Copyright © 2016 jojo mampilly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var displatText: UILabel!
+    
+    @IBAction func buttonClick(sender: UIButton) {
+    
+      displatText.text = "Button Clicked"
+
+    }
+   
+
+
+}
+
